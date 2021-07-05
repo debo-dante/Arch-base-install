df83e6e96319f83cd8c0557692bc6c8b0dd21ad8
Jiro <60966794+debo-dante@users.noreply.github.com>
2021-07-05 23:07:05 +0530

    Update kde.sh

--- kde.sh
+++ kde.sh
@@ -20,9 +20,8 @@ makepkg -si --noconfirm
 #sudo systemctl enable --now auto-cpufreq
 
 
-sudo pacman -S --noconfirm xorg sddm plasma kde-applications firefox vlc papirus-icon-theme kdenlive materia-kde
+sudo pacman -S --noconfirm xorg sddm plasma kde-applications firefox vlc papirus-icon-theme materia-kde
 
-#sudo flatpak install -y spotify
 
 sudo systemctl enable sddm
 /bin/echo -e "\e[1;32mREBOOTING IN 5..4..3..2..1..\e[0m"
