2f2888b8201e7c0b9720dfa3c6e8d2c1eca10620
Jiro <60966794+debo-dante@users.noreply.github.com>
2021-07-05 23:07:59 +0530

    Update xfce.sh

--- xfce.sh
+++ xfce.sh
@@ -22,8 +22,6 @@ makepkg -si --noconfirm
 
 sudo pacman -S --noconfirm xorg lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings xfce4 xfce4-goodies firefox arc-gtk-theme arc-icon-theme vlc
 
-#sudo flatpak install -y spotify
-sudo flatpak install -y kdenlive
 
 sudo systemctl enable lightdm
 /bin/echo -e "\e[1;32mREBOOTING IN 5..4..3..2..1..\e[0m"
